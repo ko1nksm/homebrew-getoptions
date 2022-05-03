b0954ff611b68ee9cc9c32bc5e2cfc42da26585a
danlsn <danlsn@github.com>
2022-05-03 11:44:09 +1000

    Revert "Fix: remove deprecated bottle :unneeded call"
    This reverts commit d96f556f85c180f271213d6acb2f89f9d572029a.

--- getoptions.rb
+++ getoptions.rb
@@ -4,6 +4,8 @@ class Getoptions < Formula
   url "https://github.com/ko1nksm/getoptions/archive/refs/tags/v3.3.0.tar.gz"
   sha256 "b767c6886d3b3f1258463aca500f9f06ed58f1bc6b11eb6e3e33de66847d4123"
 
+  bottle :unneeded
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
