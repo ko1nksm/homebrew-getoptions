1bcc50cccf5c9de950b76de8febeeee0e6311195
Koichi Nakashima <koichi@nksm.name>
2024-08-08 22:30:28 +0900

    Update to getoptions v3.3.1

--- getoptions.rb
+++ getoptions.rb
@@ -1,8 +1,8 @@
 class Getoptions < Formula
   desc "An elegant option parser for shell scripts"
   homepage "https://github.com/ko1nksm/getoptions"
-  url "https://github.com/ko1nksm/getoptions/archive/refs/tags/v3.3.0.tar.gz"
-  sha256 "b767c6886d3b3f1258463aca500f9f06ed58f1bc6b11eb6e3e33de66847d4123"
+  url "https://github.com/ko1nksm/getoptions/archive/refs/tags/v3.3.1.tar.gz"
+  sha256 "5873c6effd1f5ea9079bd3fa41ec098b9f0ba8a348560faf237b118982f44c93"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
